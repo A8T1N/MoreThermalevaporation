61e5d4f8e3c2714d4d9e38c52d4cb606beea5479
A8 <ryouta.pooh@icloud.com>
2026-08-31 00:26:37 +0900

    Fix:DataとGUI側でmaxMultiplierTempの整合性が取れるように修正

--- src/main/java/morethermalevaporation/client/gui/GuiMoreThermalEvaporationController.java
+++ src/main/java/morethermalevaporation/client/gui/GuiMoreThermalEvaporationController.java
@@ -67,7 +67,7 @@ public class GuiMoreThermalEvaporationController extends GuiMekanismTile<TileEnt
 
             @Override
             public double getLevel() {
-                return Math.min(1, tile.getMultiblock().getTemperature() / tier.getMultiplierTemp());
+                return Math.min(1, tile.getMultiblock().getTemperature() / (tier.getMultiplierTemp() * tile.getMultiblock().getType().getMultiplier()));
             }
         }, 48, 63))
                 //Note: We just apply this warning to the bar as we don't have an arrow or anything here
